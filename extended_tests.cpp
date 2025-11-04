4ce78a2feba296a4dd63c2c807f4ee7ce83fa9d6
Joel Singh <joelsingh788@gmail.com>
2025-11-04 10:12:58 -0500

    Add initial test for max_key

--- extended_tests.cpp
+++ extended_tests.cpp
@@ -1,2 +1,17 @@
 #include "btree_helper_functions.cpp"
-void extended_tests() {}
+#include "btree.h"
+#include <string>
+#include "test.cpp"
+
+using namespace std;
+
+const string test_path = "./tests/extended_tests/";
+
+void test_max_key() {
+  BTree tree = build_tree(test_path + "empty_tree.txt");
+  test("empty max_key returns 0", tree.max_key(tree.root), 0);
+}
+
+void extended_tests() {
+  test_max_key();
+}
