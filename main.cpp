9ac571767bcb0735eede73eca29e7aa59c5f815e
Joel Singh <joelsingh788@gmail.com>
2025-11-06 17:25:22 -0500

    Uncomment working tests

--- main.cpp
+++ main.cpp
@@ -161,8 +161,8 @@ int main() {
   test_case1(all_passed, all_total);
   test_case2a(all_passed, all_total);
   test_case2b(all_passed, all_total);
-  // test_2c(all_passed, all_total);
-  // test_3a(all_passed, all_total);
+  test_2c(all_passed, all_total);
+  test_3a(all_passed, all_total);
   // test_3b(all_passed, all_total);
 
   std::cout << "\nPassed a total of " << all_passed << "/" << all_total
